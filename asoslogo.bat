cc140518227e4bcccc4147414fa58a9506094acd
Johan Escobar Leon <johan.escobar@asos.com>
2018-08-08 10:39:06 +0100

    updated image location

--- asoslogo.bat
+++ asoslogo.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://content.asos-media.com/-/media/images/meta/asos-logo.jpg C:\Users\johan.escobar\Desktop\asos-logo.jpg
+bitsadmin.exe /transfer "test image" https://raw.githubusercontent.com/ASOSCreative/saucelabs-prerun/master/asos-logo-white.png C:\Users\Administrator\Desktop\asos-logo.jpg
